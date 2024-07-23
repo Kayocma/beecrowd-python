571c35e6a48cf1584ffee49ad1b2352d9d50d645
Kayocma <kayocma@gmail.com>
2024-07-23 00:46:04 -0300

    feat: Criar arquivo 14.consumo

---
+++ 14.consumo.py
@@ -0,0 +1,6 @@
+X = int(input()) # Recebe o valor de X
+Y = float(input()) # Recebe o valor de Y
+
+CONSUMO = X / Y # Calcula o consumo médio de combustível
+
+print("%.3f km/l" % CONSUMO) # Exibe o consumo médio de combustível com três casas decimais
