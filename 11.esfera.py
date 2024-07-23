7641398216f24ed2e1e9e4d5b312ea2325193993
Kayocma <kayocma@gmail.com>
2024-07-23 00:22:00 -0300

    feat: Criar arquivo 11.esfera

---
+++ 11.esfera.py
@@ -0,0 +1,4 @@
+pi = 3.14159 # Atribui o valor de pi a variável pi
+VOLUME = 4/3 * pi * (float(input()) ** 3) # Calcula o volume da esfera com base no raio fornecido
+
+print("VOLUME = %.3f" % VOLUME) # Exibe o volume da esfera com três casas decimais
